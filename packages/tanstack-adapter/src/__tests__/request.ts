725055b7f46ad07837cfe57bbfe3676fd9a86a4d
Nick Sethi <nicholas.sethi@gmail.com>
2025-12-02 19:47:54 -0800

    add defer and stream fixtures

--- packages/tanstack-adapter/src/__tests__/request.ts
+++ packages/tanstack-adapter/src/__tests__/request.ts
@@ -1,49 +1,43 @@
-import syncFs, { promises as fs } from 'node:fs';
+import syncFs from "node:fs";
 
-import { PatchResolver } from '~/lib/fetch-multipart/patch-resolver.ts';
-import { RelayIncrementalDeliveryTransformer } from '~/lib/relay/transformer.ts';
+import { RelayIncrementalDeliveryTransformer } from "../transformer.ts";
 
-import { executeQuery } from './schema-setup.ts';
+import { executeQuery } from "./schema-setup.ts";
 
-import {
-  parse,
-  type DocumentNode,
-  type InitialIncrementalExecutionResult,
-  type SubsequentIncrementalExecutionResult,
-} from 'graphql';
-import type { Variables } from 'relay-runtime';
+import { parse, type DocumentNode } from "graphql";
+import { graphql, type Variables } from "relay-runtime";
 
 const req = {
   id: null,
   query:
-    'query streamQuery(\n' +
-    '  $count: Int!\n' +
-    '  $cursor: String\n' +
-    ') {\n' +
-    '  alphabet(first: $count, after: $cursor) {\n' +
+    "query streamQuery(\n" +
+    "  $count: Int!\n" +
+    "  $cursor: String\n" +
+    ") {\n" +
+    "  alphabet(first: $count, after: $cursor) {\n" +
     '    edges @stream(label: "streamQuery$stream$stream_alphabet", initialCount: 0) {\n' +
-    '      cursor\n' +
-    '      ...LetterFragment\n' +
-    '      node {\n' +
-    '        __typename\n' +
-    '        id\n' +
-    '      }\n' +
-    '    }\n' +
+    "      cursor\n" +
+    "      ...LetterFragment\n" +
+    "      node {\n" +
+    "        __typename\n" +
+    "        id\n" +
+    "      }\n" +
+    "    }\n" +
     '    ... @defer(label: "streamQuery$defer$stream_alphabet$pageInfo") {\n' +
-    '      pageInfo {\n' +
-    '        hasNextPage\n' +
-    '        endCursor\n' +
-    '      }\n' +
-    '    }\n' +
-    '  }\n' +
-    '}\n' +
-    '\n' +
-    'fragment LetterFragment on AlphabetEdge {\n' +
-    '  node {\n' +
-    '    id\n' +
-    '    letter\n' +
-    '  }\n' +
-    '}\n',
+    "      pageInfo {\n" +
+    "        hasNextPage\n" +
+    "        endCursor\n" +
+    "      }\n" +
+    "    }\n" +
+    "  }\n" +
+    "}\n" +
+    "\n" +
+    "fragment LetterFragment on AlphabetEdge {\n" +
+    "  node {\n" +
+    "    id\n" +
+    "    letter\n" +
+    "  }\n" +
+    "}\n",
   variables: { count: 10, cursor: null },
 };
 
@@ -53,8 +47,10 @@ export async function complete(
   onResult: (result: any) => void,
 ) {
   const result = await executeQuery(document, variables, {});
-  const transformer = new RelayIncrementalDeliveryTransformer((v) => onResult(v));
-  if ('initialResult' in result) {
+  const transformer = new RelayIncrementalDeliveryTransformer((v) =>
+    onResult(v),
+  );
+  if ("initialResult" in result) {
     transformer.onNext([result.initialResult]);
     for await (const patch of result.subsequentResults) {
       transformer.onNext([patch]);
@@ -63,18 +59,18 @@ export async function complete(
   return result;
 }
 
-async function main() {
+async function _main() {
   const document = parse(req.query);
   const variables = req.variables;
-  const writeStream = syncFs.createWriteStream('./__tests__/transformedStream.json');
-  writeStream.write('[');
+  const writeStream = syncFs.createWriteStream(
+    "./__tests__/transformedStream.json",
+  );
+  writeStream.write("[");
   await complete(document, variables, (v) => {
     writeStream.write(JSON.stringify(v));
-    writeStream.write(',');
+    writeStream.write(",");
   }).then(() => {
-    writeStream.write(']');
+    writeStream.write("]");
     writeStream.end();
   });
 }
-
-main();
